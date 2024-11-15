1496c91a3c39d529187d1059d075485b365aed50
licheng <licheng@ic.sanhuagroup.com>
2024-11-15 14:29:43 +0800

    1

---
+++ app/src/main/java/com/attractiveboy/flower/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.attractiveboy.flower.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
---
+++ app/src/main/java/com/attractiveboy/flower/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.attractiveboy.flower.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
